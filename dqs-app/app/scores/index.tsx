3a50fce1f6987f9aa6aa78248e28cde90cf97669
cliveportman <clive@theportman.co>
2024-10-27 16:45:51 +0000

    refactor: moving some constants into local route directory

--- dqs-app/app/scores/index.tsx
+++ dqs-app/app/scores/index.tsx
@@ -1,47 +1,13 @@
 import database from "@/core/database";
-import {PossibleSingleServingScores, Servings} from "@/core/types";
+import { Servings} from "@/core/types";
 import {FoodCat} from "@/core/enums";
 import {useEffect, useState} from "react";
 import {SQLiteDatabase} from "expo-sqlite";
 
-import {TwContainer} from "@/core/components/TwContainer"
-import {TwText} from "@/core/components/TwText"
+import { defaultServings, maxScores } from '@/app/scores/constants';
+import {TwContainer} from "@/core/components/TwContainer";
+import {TwText} from "@/core/components/TwText";
 import {Score} from "@/app/scores/components/Score";
-
-// Maximum possible scores for each food category, if a user eats the max number of servings (6).
-const maxScores = {
-    veg: [2, 2, 2, 1, 0, 0] as PossibleSingleServingScores,
-    fruit: [2, 2, 2, 1, 0, 0] as PossibleSingleServingScores,
-    nuts: [2, 2, 1, 0, 0, -1] as PossibleSingleServingScores,
-    wholegrains: [2, 2, 1, 0, 0, -1] as PossibleSingleServingScores,
-    dairy: [2, 1, 1, 0, -1, -2] as PossibleSingleServingScores,
-    leanproteins: [2, 1, 1, 0, -1, -2] as PossibleSingleServingScores,
-    beverages: [1, 1, 0, 0, 0, 0] as PossibleSingleServingScores,
-    refinedgrains: [-1, -1, -2, -2, -2, -2] as PossibleSingleServingScores,
-    sweets: [-2, -2, -2, -2, -2, -2] as PossibleSingleServingScores,
-    fattyproteins: [-2, -2, -2, -2, -2, -2] as PossibleSingleServingScores,
-    friedfoods: [-2, -2, -2, -2, -2, -2] as PossibleSingleServingScores,
-    alcohol: [-2, -2, -2, -2, -2, -2] as PossibleSingleServingScores,
-    other: [-1, -2, -2, -2, -2, -2] as PossibleSingleServingScores,
-}
-
-const defaultServings: Servings = {
-    date: '',
-    veg: 0,
-    fruit: 0,
-    nuts: 0,
-    wholegrains: 0,
-    dairy: 0,
-    leanproteins: 0,
-    beverages: 0,
-    refinedgrains: 0,
-    sweets: 0,
-    fattyproteins: 0,
-    friedfoods: 0,
-    alcohol: 0,
-    other: 0,
-};
-
 
 export default function App() {
     
