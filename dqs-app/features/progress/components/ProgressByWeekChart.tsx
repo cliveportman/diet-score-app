ff7a0e33684a741be9263ed71a959ec54379bdf8
cliveportman <clive@theportman.co>
2024-11-22 03:38:59 +0000

    feat: release number

--- dqs-app/features/progress/components/ProgressByWeekChart.tsx
+++ dqs-app/features/progress/components/ProgressByWeekChart.tsx
@@ -1,4 +1,4 @@
-import {DateString, Servings} from "@/core/types";
+import {DateString } from "@/core/types";
 import React, {useEffect} from 'react';
 import { TwContainer } from "@/core/components/TwContainer";
 import { format } from 'date-fns';
@@ -17,19 +17,6 @@ type ProgressByWeekChartProps = {
 type BarChartData = {
     data: BarData[];
     title: string;
-}
-
-function mapDataToDates(data: Servings[], dates: DateString[]): BarChartData {
-    return {
-        data: dates.map((date) => {
-            const day = data.find((item) => item.date === date);
-            return {
-                value: day ? getTotalScoresForMaths(day).total : 0,
-                label: format(date, 'EEE'),
-            };
-        }),
-        title: `${format(dates[0], 'EEE dd MMM')} to ${format(dates[dates.length - 1], 'EEE dd MMM')}`,
-    }
 }
 
 export function ProgressByWeekChart({ week }: ProgressByWeekChartProps) {
@@ -55,10 +42,10 @@ export function ProgressByWeekChart({ week }: ProgressByWeekChartProps) {
     }
 
     return (
-        <TwContainer twc={"mb-6"} style={{ width: width - 24 }}>
+        <TwContainer style={{ width: width - 24 }}>
             <TwText variant={"subtitle"} twc={"text-left mb-3"}>{format(week[0], 'dd MMM')} - {format(week[6], 'dd MMM')}</TwText>
             <TwContainer twc={"relative mb-6"} style={{ height: 220 }}>
-                <TwContainer twc={"absolute left-0 top-0 right-0 bottom-0 mt-6 bg-slate-950"}>
+                <TwContainer twc={"absolute left-0 top-0 right-0 bottom-0 mt-6"}>
                     <TwContainer twc={"absolute w-full bg-slate-900"} style={{ top: 2/42 * 200, height: 1 }} />
                     <TwContainer twc={"absolute w-full bg-slate-900"} style={{ top: 12/42 * 200, height: 1 }} />
                     <TwContainer twc={"absolute w-full bg-slate-900"} style={{ top: 22/42 * 200, height: 1 }} />
