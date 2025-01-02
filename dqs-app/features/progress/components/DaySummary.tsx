e5aa9086d17a7ff05f429861f902c9a9ebcfa23b
Clive Portman <clive@theportman.co>
2025-01-02 20:22:11 +0000

    wip: link

--- dqs-app/features/progress/components/DaySummary.tsx
+++ dqs-app/features/progress/components/DaySummary.tsx
@@ -3,13 +3,17 @@ import React from 'react';
 import { TwContainer } from "@/core/components/TwContainer"
 import { TwText } from "@/core/components/TwText"
 import { getTotalScoresForDisplay } from "@/core/helpers";
+import { useRouter } from "expo-router";
 import dayjs from "dayjs";
+import {Pressable} from "react-native";
 
 type DaySummaryProps = {
     data: Servings;
 }
 
 export function DaySummary({data}: DaySummaryProps) {
+
+    const router = useRouter();
     
     const totals = getTotalScoresForDisplay(data);
     
@@ -25,7 +29,7 @@ export function DaySummary({data}: DaySummaryProps) {
     
     return (
         <TwContainer twc={"flex-row justify-between items-start bg-slate-900 px-3 pt-1.5 mb-1.5"}>
-            <TwText twc={"w-28 leading-none h-5 text-slate-300"}>{dayjs(data.date).format('ddd DD MMM')}</TwText>
+            <Pressable onPress={() => router.push(`(app)/scores/${dayjs(data.date).format('YYYY-MM-DD')}`)}><TwText twc={"w-28 leading-none h-5 text-slate-300"}>{dayjs(data.date).format('ddd DD MMM')}</TwText></Pressable>
             <TwText twc={`w-16 leading-none h-5 text-slate-300 text-center`}>{totals.portions}</TwText>
             <TwText twc={`w-10 leading-none h-5 text-slate-300 text-right`}>{totals.healthy}</TwText>
             <TwText twc={`w-16 leading-none h-5 text-slate-300 text-right`}>{totals.unhealthy}</TwText>
