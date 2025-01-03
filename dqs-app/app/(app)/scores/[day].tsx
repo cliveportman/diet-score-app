c92d72579080837859b105b96ce2b18b05325ed3
Clive Portman <clive@theportman.co>
2025-01-03 04:24:48 +0000

    feat: routing to specific day

--- dqs-app/app/(app)/scores/[day].tsx
+++ dqs-app/app/(app)/scores/[day].tsx
@@ -1,52 +1,45 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { useLocalSearchParams } from "expo-router";
 
 import {TwContainer} from "@/core/components/TwContainer";
 import { Day } from "@/features/scores/components/Day";
 import { Dimensions, FlatList, View} from "react-native";
 import {useDatabase} from "@/core/hooks";
+import dayjs from 'dayjs';
 
 export default function Scores() {
 
     const db = useDatabase();
     const { day } = useLocalSearchParams<{ day: string }>();
-    const flatListRef = useRef<FlatList | null>(null);
 
     const { width } = Dimensions.get('window');
-    const [days, setDays] = useState([
-        {date: new Date(new Date().setDate(new Date(day).getDate() + 1))},
-        {date: new Date(day)},
-        {date: new Date(new Date().setDate(new Date(day).getDate() - 1))}
-    ]);
+    const [days, setDays] = useState(() => {
+        /**
+         * Because this screen is referring to a specific date,
+         * we're setting the initial state to be a list of all the
+         * dates from the selected date to today's date.
+         * This performs much better than sticking with just a date either side and
+         * using onStartReached.
+         * It's not an issue because it's only a list of dates anyway.
+         */
+        const today = dayjs();
+        const selectedDay = dayjs(day);
+        const diffInDays = today.diff(selectedDay, 'day');
+        const daysArray = [];
+        for (let i = diffInDays; i >= 0; i--) {
+            daysArray.push({ date: selectedDay.add(+i, 'day').toDate() }); // Use dayjs.add
+        }
+        return daysArray;
+    });
 
-    function handleStartReached() {
-        const firstDate = days[0].date;
-        const newDate = new Date(new Date().setDate(firstDate.getDate() + 1));
-        setDays([{date: newDate}, ...days]);
-        if (flatListRef.current) flatListRef.current.scrollToIndex({ index: 1, animated: false });
-
-    }
-    
     function handleEndReached() {
         const lastDate = new Date(days[days.length - 1].date);
         const newDate = new Date(lastDate.setDate(lastDate.getDate() - 1));
-        //setDays([...days, {date: newDate}]);
-
-        setDays(prevDays => {
-            const newDays = [{ date: newDate }, ...prevDays];
-            setTimeout(() => {
-                if (flatListRef.current) {
-                    flatListRef.current.scrollToIndex({ index: 1, animated: false }); // Now type-safe
-                }
-            }, 0);
-            return newDays;
-        });
+        setDays([...days, {date: newDate}]);
     }
 
-
-        
     return (
-        <TwContainer twc="flex-1 flex-col justify-center bg-slate-950">            
+        <TwContainer twc="flex-1 flex-col justify-center bg-slate-950">
             <View tw="flex-1">
                 {(db && days.length) && (
                 <FlatList
@@ -57,16 +50,13 @@ export default function Scores() {
                     inverted
                     snapToInterval={width}
                     decelerationRate="fast"
-                    initialScrollIndex={1}
+                    initialScrollIndex={days.length - 1}
                     onEndReached={handleEndReached}
-                    onStartReached={handleStartReached}
                     onEndReachedThreshold={0.5}
-                    onStartReachedThreshold={0.5}
                     extraData={days}
                     getItemLayout={(_, index) => (
                         { length: width, offset: width * index, index}
                     )}
-                    ref={flatListRef}
                 />
                 )}
             </View>
